b7be82fdbff537b4d9c029530fc3d3a9b15c357f
Mohsin Khan <0xmohsinpathan@gmail.com>
2023-03-07 12:28:30 +0530

    🔨 Refactor Code:  my JS code more simple.

--- project.js
+++ project.js
@@ -1,83 +1,28 @@
-let runningTotal = 0;
-let buffer = "0";
-let previousOperator = null;
-const screen = document.querySelector(".screen");
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll
+('button');
 
-document.querySelector(".calc-button").addEventListener("click", function(event){       
-    buttonClick(event.target.innerText);
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '=') {
+        string = eval(string);
+        input.value = string;
+    }
+
+    else if(e.target.innerHTML == 'AC'){
+        string = "";
+        input.value = string;
+    }
+    else if(e.target.innerHTML == 'DEL'){
+        string = string.substring(0, string.length-1);
+        input.value = string;
+    }
+    else{
+        string += e.target.innerHTML;
+          input.value = string;
+    }
+
+    })
 })
-
-function buttonClick(value) {
-    if (isNaN(parseInt(value))) {
-        handleSymbol(value);
-    } else {
-        handleNumber(value);
-    }
-    rerender();
-}
-
-function handleNumber(value) {
-    if (buffer === "0") {
-        buffer = value;
-    } else {
-        buffer += value;    
-    }    
-}
-
-function handleSymbol(value) {
-    switch (value) {
-        case 'AC':
-            buffer = "0";
-            runningTotal = 0;
-            previousOperator = null;
-            break;
-        case "=":
-            if (previousOperator === null) {
-                return;               
-            }
-            flushOperation(parseInt(buffer));
-            previousOperator = null;
-            buffer = "" + runningTotal; 
-            runningTotal = 0;
-            break;
-        case "←":
-            if (buffer.length === 1) {
-                buffer = "0";
-            } else {
-                buffer = buffer.substring(0, buffer.length - 1);
-            }
-            break;
-        default:
-                handleMath(value);
-                break;                     
-    }
-}
-
-function handleMath(value) {
-    const intBuffer = parseInt(buffer);
-    if (runningTotal === 0) {
-        runningTotal = intBuffer;
-    } else {
-        flushOperation(intBuffer);
-    }
-    
-    previousOperator = value;
-
-    buffer = "0";
-}
-
-function flushOperation (intBuffer) {
-    if (previousOperator === "+") {
-        runningTotal += intBuffer;
-    } else if (previousOperator === "-") {
-        runningTotal -= intBuffer;
-    } else if (previousOperator === "×") {
-        runningTotal *= intBuffer;
-    } else {
-        runningTotal /= intBuffer;
-    }
-}
-
-function rerender() {
-    screen.innerText = buffer;
-}
